9aed43af2b64f9722c3d95a36592c8cbc9dc864b
Foxhkron <foxhkron@toromino.de>
2019-03-17 14:18:49 +0100

    Format commits using rustfmt

--- src/bin/create_user.rs
+++ src/bin/create_user.rs
@@ -8,20 +8,34 @@ use kibou::database;
 use kibou::env;
 use std::env::args;
 
-fn main ()
-{
+fn main() {
     let database = database::establish_connection();
     let args: Vec<String> = std::env::args().collect();
 
     let mut options = Options::new();
-    options.reqopt("e", "email", "e-mail address for the new user", "alyssa@example.com");
-    options.reqopt("p", "password", "password for the new user", "MySecretPassword");
+    options.reqopt(
+        "e",
+        "email",
+        "e-mail address for the new user",
+        "alyssa@example.com",
+    );
+    options.reqopt(
+        "p",
+        "password",
+        "password for the new user",
+        "MySecretPassword",
+    );
     options.reqopt("u", "username", "username for the new user", "alyssatest");
-    options.optopt("s", "summary", "a summary (bio) for the new user", "I am a Kibou Test-Actor, how are you doing?");
+    options.optopt(
+        "s",
+        "summary",
+        "a summary (bio) for the new user",
+        "I am a Kibou Test-Actor, how are you doing?",
+    );
 
     let matches = match options.parse(&args[1..]) {
-        Ok(m) => { m }
-        Err(e) => { panic!(e.to_string()) }
+        Ok(m) => m,
+        Err(e) => panic!(e.to_string()),
     };
 
     let mut new_actor = actor::Actor {
@@ -32,7 +46,8 @@ fn main ()
             "{base_scheme}://{base_domain}/actors/{username}",
             base_scheme = env::get_value(String::from("endpoint.base_scheme")),
             base_domain = env::get_value(String::from("endpoint.base_domain")),
-            username = matches.opt_str("username").unwrap()),
+            username = matches.opt_str("username").unwrap()
+        ),
         username: Some(matches.opt_str("username").unwrap()),
         preferred_username: matches.opt_str("username").unwrap(),
         summary: matches.opt_str("summary"),
@@ -40,7 +55,7 @@ fn main ()
         inbox: None,
         icon: None,
         local: true,
-        keys: serde_json::json!({})
+        keys: serde_json::json!({}),
     };
 
     actor::create_actor(&database, &mut new_actor)
