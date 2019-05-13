f8c185cb16340f67d607630f115e2abcb042ff5c
Toromino <foxhkron@toromino.de>
2019-05-13 18:36:53 +0200

    Escape raw sql queries

--- src/activity.rs
+++ src/activity.rs
@@ -1,4 +1,5 @@
 use database::models::{InsertActivity, QueryActivity};
+use database::runtime_escape;
 use database::schema::activities;
 use database::schema::activities::dsl::*;
 use diesel::pg::PgConnection;
@@ -21,7 +22,7 @@ pub fn count_ap_object_replies_by_id(
 ) -> Result<usize, diesel::result::Error> {
     match sql_query(format!(
         "SELECT * FROM activities WHERE data->'object'->>'inReplyTo' = '{}';",
-        object_id
+        runtime_escape(object_id)
     ))
     .clone()
     .load::<QueryActivity>(db_connection)
@@ -37,8 +38,8 @@ pub fn count_ap_object_reactions_by_id(
     reaction: &str,
 ) -> Result<usize, diesel::result::Error> {
     match sql_query(format!("SELECT * FROM activities WHERE data->>'type' = '{reaction_type}' AND data->>'object'= '{id}';",
-                            reaction_type = reaction,
-                            id = object_id))
+                            reaction_type = runtime_escape(reaction),
+                            id = runtime_escape(object_id)))
         .load::<QueryActivity>(db_connection)
         {
             Ok(activity_arr) => Ok(activity_arr.len()),
@@ -58,7 +59,7 @@ pub fn count_ap_notes_for_actor(
          AND data->>'actor' = '{actor}' \
          AND ((data->>'to')::jsonb ? 'https://www.w3.org/ns/activitystreams#Public' \
          OR (data->>'cc')::jsonb ? 'https://www.w3.org/ns/activitystreams#Public');",
-        actor = actor
+        actor = runtime_escape(actor)
     ))
     .load::<QueryActivity>(db_connection)
     {
@@ -107,7 +108,7 @@ pub fn get_ap_activity_by_id(
 ) -> Result<Activity, diesel::result::Error> {
     match sql_query(format!(
         "SELECT * FROM activities WHERE data->>'id' = '{}' LIMIT 1;",
-        activity_id
+        runtime_escape(activity_id)
     ))
     .clone()
     .load::<QueryActivity>(db_connection)
@@ -130,7 +131,7 @@ pub fn get_ap_object_by_id(
 ) -> Result<Activity, diesel::result::Error> {
     match sql_query(format!(
         "SELECT * FROM activities WHERE data->'object'->>'id' = '{}' LIMIT 1;",
-        object_id
+        runtime_escape(object_id)
     ))
     .clone()
     .load::<QueryActivity>(db_connection)
@@ -153,7 +154,7 @@ pub fn get_ap_object_replies_by_id(
 ) -> Result<Vec<Activity>, diesel::result::Error> {
     match sql_query(format!(
         "SELECT * FROM activities WHERE data->'object'->>'inReplyTo' = '{}';",
-        object_id
+        runtime_escape(object_id)
     ))
     .clone()
     .load::<QueryActivity>(db_connection)
@@ -198,7 +199,7 @@ pub fn insert_activity(db_connection: &PgConnection, activity: Activity) {
 pub fn delete_ap_activity_by_id(db_connection: &PgConnection, activity_id: String) {
     sql_query(format!(
         "DELETE FROM activities WHERE data->>'id' = '{}';",
-        activity_id
+        runtime_escape(&activity_id)
     ))
     .execute(db_connection);
 }
@@ -206,7 +207,7 @@ pub fn delete_ap_activity_by_id(db_connection: &PgConnection, activity_id: Strin
 pub fn delete_ap_object_by_id(db_connection: &PgConnection, object_id: String) {
     sql_query(format!(
         "DELETE FROM activities WHERE data->'object'->>'id' = '{}';",
-        object_id
+        runtime_escape(&object_id)
     ))
     .execute(db_connection);
 }
